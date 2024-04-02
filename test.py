f89c9ffb822db9c2b2c4d646c7a2faa04aaca775
dwhuang219 <82177072+dwhuang219@users.noreply.github.com>
2024-04-02 11:41:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
     url = f"https://api.github.com/repos/{repo_name}"
     logger = get_run_logger()
     name = get_name()
+    print("changed: version1")
     repo = {"1": 1, "2": 2}
     logger.info("%s repository statistics 🤓:", repo_name)
     logger.info(f"Stars 🌠 : %d", repo["1"])
